96e5f24a4ee0fab4c3da49731b112edfa5a585da
colinamiguel <miguelcolina@correo.unimet.edu.ve>
2024-03-09 01:02:54 -0400

    Responsive saved for later

--- src/Components/SavedMovie.jsx
+++ src/Components/SavedMovie.jsx
@@ -19,16 +19,16 @@ export default function SavedMovie({ movie, user, setUser }){
         <div className="flex flex-col col-span-2 font-roboto">
             <p className="text-white text-[15px] font-semibold tracking-wide cursor-default mb-[5px]">{movie.name}</p>
             <div className='col-span-2 flex gap-x-[20px]'>
-                <img className='shadow-3xl w-[100px] h-[150px] col-span-1 object-cover' src={movie.image}/>
+                <img className='shadow-3xl xl:w-[100px] lg:w-[100px] md:w-[120px] sm:w-[120px] xs:w-[100px] xl:h-[150px] lg:h-[150px] md:h-[165px] sm:h-[170px] col-span-1 object-cover' src={movie.image}/>
                 <div className='col-span-1 flex flex-col justify-between h-5/6'>
                     <p className="text-[#828282] text-[12px] text-wrap cursor-default font-semibold">{`${movie.language} ${movie.duration} ${movie.genre}`}</p>
                     <div className="flex text-center cursor-default content-center gap-x-[4px] font-semibold">
                         <p className="text-[#828282] text-center text-[14px]">Rating: <span className="text-[#A70000]">{movie.rating}/5</span></p>
-                        <p className="text-[#A70000] h-full mt-[1px]">{<IoStarSharp />}</p>
+                        <p className="text-[#A70000] h-full mt-[1px] lg:visible md:visible sm:visible xs:invisible">{<IoStarSharp />}</p>
                     </div>
-                    <div className='flex gap-x-[20px]'>
-                        <button className="border-solid w-full border-[1px] border-white hover:border-[1px] hover:border-[#A70000] transition-[bg_200,border_200] duration-200 z-20 bg-transparent text-white hover:bg-[#A70000] hover:border-transparent hover:text-white text-[12px] px-[12px] py-[2px] text-center"><Link to={`/movie/${movie._id}`}>Play</Link></button>
-                        <button onClick={()=>{handleRemove()}} className="border-solid w-full border-[1px] border-white hover:border-[1px] hover:border-[#A70000] transition-[bg_200-border_200] duration-200 z-20 bg-transparent text-white hover:bg-[#A70000] hover:border-transparent hover:text-white text-[12px] px-[12px] py-[2px] text-center">Remove</button>
+                    <div className='flex gap-x-[20px] xl:flex-row lg:flex-row md:flex-row sm:flex-row xs:flex-col xs:gap-y-[10px]'>
+                        <button className="border-solid w-full border-[1px] border-white hover:border-[1px] hover:border-[#A70000] transition-[bg_200,border_200] duration-200 z-20 bg-transparent text-white hover:bg-[#A70000] hover:border-transparent hover:text-white text-[11px] xl:px-[12px] lg:px-[12px] md:px-[12px] sm:px-[12px] xs:px-[0px] py-[2px] text-center xl:w-full lg:w-full md:w-full sm:w-full xs:w-4/6"><Link to={`/movie/${movie._id}`}>Play</Link></button>
+                        <button onClick={()=>{handleRemove()}} className="border-solid w-full border-[1px] border-white hover:border-[1px] hover:border-[#A70000] transition-[bg_200-border_200] duration-200 z-20 bg-transparent text-white hover:bg-[#A70000] hover:border-transparent hover:text-white text-[11px] xl:px-[12px] lg:px-[12px] md:px-[12px] sm:px-[12px] xs:px-[0px] py-[2px] text-center xl:w-full lg:w-full md:w-full sm:w-full xs:w-4/6">Remove</button>
                     </div>
                 </div>
             </div>
--- src/Components/SavedTab.jsx
+++ src/Components/SavedTab.jsx
@@ -4,11 +4,11 @@ export default function SavedTab({ user, setUser }){
 
     return(
         <div className="grid grid-cols-10 col-span-10 col-start-2">
-            <div className="flex col-span-2 col-start-2 mt-[50px] mb-[40px]">
+            <div className="flex col-span-8 col-start-2 mt-[50px] mb-[40px]">
                 <div className='bg-[#A70000] my-auto h-[20px] w-[7px]' />
                 <p className="pl-[10px] my-auto cursor-default text-[24px] font-bold text-center text-white">Saved for later</p>
             </div>
-            <div className="grid grid-cols-8 col-span-8 col-start-2 gap-y-[30px] gap-x-[20px] overflow-y-scroll h-[400px] scrollbar scrollbar-w-[2px] scrollbar-h-[2px] scrollbar-rounded-sm scrollbar-thumb-[#A7000055] scrollbar-opacity-50">
+            <div className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 col-span-8 col-start-2 xl:gap-y-[30px] lg:gap-y-[30px] md:gap-y-[10px] sm:gap-y-[10px] xs:gap-y-[10px] gap-x-[20px] overflow-y-scroll h-[400px] scrollbar scrollbar-w-[2px] scrollbar-h-[2px] scrollbar-rounded-sm scrollbar-thumb-[#A7000055] scrollbar-opacity-50">
                 {user.saved.length === 0 ? 
                     (<div className="text-center h-[50px] col-span-8 col-start-1 my-auto text-white font-roboto font-semibold text-[18px]">
                         There are no saved movies to display...
